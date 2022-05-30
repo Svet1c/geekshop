30cdc74c3f275b2408faa2bdd7b6ce7e33e5e2a2
Svet <utug2004@gmail.com>
2022-05-30 18:29:59 +0300

    lesson_3

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from mainapp import views
 
 urlpatterns = [
@@ -22,12 +22,7 @@ urlpatterns = [
 
     path('', views.index, name='index'),
 
-    path('products/', views.products, name='products'),
-    path('products/all/', views.products, name='products_all'),
-    path('products/home/', views.products, name='products_home'),
-    path('products/office/', views.products, name='products_office'),
-    path('products/modern/', views.products, name='products_modern'),
-    path('products/classic/', views.products, name='products_classic'),
+    path('products/', include('mainapp.urls', namespace='products')),
 
     path('contact/', views.contact, name='contact'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+from mainapp.models import Product, Category
 
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'products': Product.objects.all()[:4]
+    }
+    return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
-    links_menu = [
-        {'href': 'products_all', 'title': 'все'},
-        {'href': 'products_home', 'title': 'дом'},
-        {'href': 'products_office', 'title': 'офис'},
-        {'href': 'products_modern', 'title': 'модерн'},
-        {'href': 'products_classic', 'title': 'классика'}
-    ]
+
     context = {
-        'links_menu': links_menu,
+        'links_menu': Category.objects.all(),
         'title': 'Каталог'
+    }
+    return render(request, 'mainapp/products.html', context)
+
+
+def products_list(request, pk):
+    context = {
+        'links_menu': Category.objects.all(),
     }
     return render(request, 'mainapp/products.html', context)
 
