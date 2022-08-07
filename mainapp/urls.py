a15e7346b348db78d6a1e31bfeedc33787f1b940
Svet <utug2004@gmail.com>
2022-08-07 22:21:20 +0300

    lesson_6

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from mainapp.views import products
+from mainapp.views import products, product
 
 app_name = 'products'
 
-
 urlpatterns = [
     path('', products, name='products_hot_product'),
-    path('<int:pk>/', products, name='product_list')
+    path('<int:pk>/', products, name='product_list'),
+    path('product/<int:pk>/', product, name='product_item')
 ]
